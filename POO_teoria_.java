13b6d04ae768cd085b1eee1c7d7ea1a828a2867e
ku1lef1x <47606966+ku1lef1x@users.noreply.github.com>
2019-03-10 19:11:31 +0100

    Add files via upload

---
+++ POO_teoria_.java
@@ -0,0 +1,61 @@
+package POO;
+
+public class POO_teoria_ {
+
+	/*La programacion, se puede distinguir a grandes rasgos en dos bloques:
+	 * 
+	 * 		a. Programacion orientada a procedimientos.
+	 * 		b. Programacion orientada a objetos.
+	 * 
+	 * La prog orientada a procedimientos daba lugar a códigos demasiado extensos, dificiles de depurar, no reutilizables....
+	 * por tanto, decidio darse un giro a la manera de plantear la programacion. Se decidio observar el comportamiento de los 
+	 * objetos de la vida real, para asi simplificar la programacion y que una persona humana pudiera añadir facilmente
+	 * nuevas funciones, buscar fallos...
+	 * 
+	 * Como comentabamos, la prog orientada a procedimientos da lugar a codigos muy extensos, dificiles de descifrar,
+	 * poco reutilizables... Ademas, habia que dar muchos saltos en el flujo de programacion y un fallo en una sola de
+	 * sus lineas hacia que todo el programa se cayera.
+	 * 
+	 * La POO traslada la naturaleza de los objetos de la vida real al codigo de programacion. 
+	 * Todos los objetos, a grandes rasgos, tienen tres cosas en común:
+	 * 
+	 * 		1. Estado.
+	 * 		2. Propiedas.
+	 * 		3. Comportamiento.
+	 * 
+	 * Por ejemplo, un coche:
+	 * 
+	 * 		Estado:parado, andando, corriendo...
+	 * 		Propiedades: color, peso, largo, tamaño...
+	 * 		Comportamiento: arrancar, frenar, acelerar...
+	 * 
+	 * La POO permite dividir los programas en partes, trozos, modulos... Por ejemplo, el coche no es mas
+	 * que la suma de otros objetos como son las ruedas, el motor, el chasis...
+	 * La POO hace que losprogramas sean mas reutilizables gracias a la herencia. Si existe algun fallo en alguna
+	 * linea el programa continuara su funcionamiento. 
+	 * Se pueden tratar excepciones y usar el encapsulamiento.
+	 * 
+	 * MODULARIZACION: Dividir el programa en partes o trozos que finalmente se conectaran entre si
+	 * para formar un todo. Un error en uno de los modulos o partes, dara lugar a que ese modulo no funcione
+	 * pero el resto funcionaran perfectamente. 
+	 * 
+	 * HERENCIA: las caracteristicas de un objeto son reutilizables en otro objeto similar
+	 * 
+	 * TRATAMIENTO DE EXCEPCIONES: si existe un fallo el programa sigue, por ejemplo el coche
+	 * si tiene algun fallo en algun componente no deja de funcionar de golpe
+	 * 
+	 * ENCAPSULAMIENTO: si por ejemplo en el coche tenemos la caja de cambio, ésta no tiene que conocer
+	 * el funcionamiento de la rueda trasera, ni sabe como esta construida, ni viceversa. Se pueden comunicar en algun
+	 * momento pero son objetos totalmente independientes el uno del otro. 
+	 * Los objetos se uniran en armonia posteriormente, pero todos son independientes unos de otros.
+	 * 
+	 * Un programa realizado basandose en la POO dará lugar a un grupo de clases unidas entre si de una u otra forma
+	 * que hara que el programa funcione como una unica unidad. 
+	 * Los programas que hemos elaborado hasta ahora constaban de una sola clase, a partir de ahora, los programas, por muy 
+	 * sencillos que sean constaran de minimo dos clases que estaran conectadas entre ellas. 
+	 * 
+	 * Debemos tener en cuentas los conceptos: CLASE, OBJETO, EJEMPLAR DE CLASE(instancia de clase, ejemplarizar una clase,
+	 * instanciar una clase...), MODULARIZACION, ENCAPSULAMIENTO O ENCAPSULACION, HERENCIA, POLIMORFISMO... que se iran 
+	 * viendo de aquí en adelante.*/
+
+}
